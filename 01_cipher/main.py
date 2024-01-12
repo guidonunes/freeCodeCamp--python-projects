e690b0ee5042a5b388c956534d3a1e968135fbc4
guilherme2506 <guilherme.augd@gmail.com>
2024-01-12 15:48:01 -0300

    declared & print shifted varialble

--- 01_cipher/main.py
+++ 01_cipher/main.py
@@ -1,5 +1,7 @@
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0])
+index = alphabet.find(text[0].lower())
 print(index)
+shifted = alphabet[index + shift]
+print(shifted)
