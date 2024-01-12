557fcff23338a3eb125b34b051e21f9df19406a6
guilherme2506 <guilherme.augd@gmail.com>
2024-01-12 16:38:50 -0300

    updated variable name in the for loop

--- 01_cipher/main.py
+++ 01_cipher/main.py
@@ -1,7 +1,6 @@
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0].lower())
-print(index)
-shifted = alphabet[index + shift]
-print(shifted)
+
+for char in text: 
+    print(char)
