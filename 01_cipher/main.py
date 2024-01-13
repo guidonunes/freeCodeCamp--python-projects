c1d99f2c82b0ed6d4731d0bf106ff915ad5d4e8b
guilherme2506 <guilherme.augd@gmail.com>
2024-01-13 09:47:44 -0300

    added new_char var at the end of the for loop body

--- 01_cipher/main.py
+++ 01_cipher/main.py
@@ -4,5 +4,6 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 for char in text.lower(): 
     index = alphabet.find(char)
-    new_index = index + shift
     print(char, index)
+    new_index = index + shift
+    new_char = alphabet[new_index]
