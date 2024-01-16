66f62ba5c9fb24a0c6a5e160c622406e09410559
guilherme2506 <guilherme.augd@gmail.com>
2024-01-16 18:05:57 -0300

    fixed code indentation

--- 01_cipher/main.py
+++ 01_cipher/main.py
@@ -1,6 +1,7 @@
+text = 'Hello Zaira'
+shift = 3
+
 def caesar(message, offset):
-    message = 'Hello Zaira'
-    offset = 3
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
@@ -11,7 +12,6 @@ def caesar(message, offset):
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
-
     print('plain text:', message)
     print('encrypted text:', encrypted_text)
 
