fd1a85b522586726bcd36e11078af3cf57ee4e70
guilherme2506 <guilherme.augd@gmail.com>
2024-01-16 10:18:20 -0300

    unindented the print call & put it outside the for loop

--- 01_cipher/main.py
+++ 01_cipher/main.py
@@ -3,11 +3,12 @@ shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 encrypted_text = ''
 
-for char in text.lower(): 
+for char in text.lower():
     if char == ' ':
         encrypted_text += char
     else:
         index = alphabet.find(char)
         new_index = (index + shift) % len(alphabet)
         encrypted_text += alphabet[new_index]
-        print('char:', char, 'encrypted text:', encrypted_text)
+
+print('encrypted text:', encrypted_text)
