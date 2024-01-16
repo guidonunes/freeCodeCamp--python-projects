31aa82c5492f7e932e31534e4ba6e9d9ab731c12
guilherme2506 <guilherme.augd@gmail.com>
2024-01-16 18:02:26 -0300

    renamed text and shift variables to message and offset

--- 01_cipher/main.py
+++ 01_cipher/main.py
@@ -1,19 +1,18 @@
-text = 'Hello Zaira'
-shift = 3
-
 def caesar(message, offset):
+    message = 'Hello Zaira'
+    offset = 3
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
-    for char in text.lower():
+    for char in message.lower():
         if char == ' ':
             encrypted_text += char
         else:
             index = alphabet.find(char)
-            new_index = (index + shift) % len(alphabet)
+            new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
 
-    print('plain text:', text)
+    print('plain text:', message)
     print('encrypted text:', encrypted_text)
 
 caesar()
