2679c61397d8e8f772fe2eaa7c237eac00065038
guilherme2506 <guilherme.augd@gmail.com>
2024-01-16 10:11:22 -0300

    implemented modulo operator on the new_index variable

--- 01_cipher/main.py
+++ 01_cipher/main.py
@@ -1,4 +1,4 @@
-text = 'Hello World'
+text = 'Hello Zaira'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 encrypted_text = ''
@@ -8,6 +8,6 @@ for char in text.lower():
         encrypted_text += char
     else:
         index = alphabet.find(char)
-        new_index = index + shift
+        new_index = (index + shift) % 26
         encrypted_text += alphabet[new_index]
         print('char:', char, 'encrypted text:', encrypted_text)
