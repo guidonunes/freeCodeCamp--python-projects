4b30826e3c1b201495514cf5333ae82095683e21
guilherme2506 <guilherme.augd@gmail.com>
2024-01-17 16:31:37 -0300

    declaredoffset variable within the else block

--- 01_cipher/main.py
+++ 01_cipher/main.py
@@ -14,6 +14,7 @@ def vigenere(message, key):
              #Find the right key character to encode    
             key_char = key[key_index % len(key)]
             key_index += 1
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + key) % len(alphabet)
             encrypted_text += alphabet[new_index]
