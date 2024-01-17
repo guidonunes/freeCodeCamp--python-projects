8cbaf069338ff2ef83fc2d99e1eb249c41127918
guilherme2506 <guilherme.augd@gmail.com>
2024-01-17 15:54:06 -0300

    added a comment after the else clause

--- 01_cipher/main.py
+++ 01_cipher/main.py
@@ -11,7 +11,9 @@ def vigenere(message, key):
         if char == ' ':
             encrypted_text += char
         else:
+             #Find the right key character to encode    
             key_char = key[key_index % len(key)]
+            key_index += 1
             index = alphabet.find(char)
             new_index = (index + key) % len(alphabet)
             encrypted_text += alphabet[new_index]
