c32871edbe40874d0548ad040b440e5b7dd049ac
guilherme2506 <guilherme.augd@gmail.com>
2024-01-22 17:42:05 -0300

    called verified_card_number within the main function

--- 02_luhn_algorithm/main.py
+++ 02_luhn_algorithm/main.py
@@ -5,5 +5,6 @@ def main():
     card_number='4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '',' ': ''})
     translated_card_number = card_number.translate(card_translation)
+    verify_card_number(translated_card_number)
     print(translated_card_number)
 main()
