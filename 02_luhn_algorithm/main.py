03c0cf161e32f82aac591b9f6297824542f6171b
guilherme2506 <guilherme.augd@gmail.com>
2024-01-23 20:58:45 -0300

    removed print call within the if statement

--- 02_luhn_algorithm/main.py
+++ 02_luhn_algorithm/main.py
@@ -11,8 +11,7 @@ def verify_card_number(card_number):
         number = int(digit) * 2
         if number >= 10:
             number = number // 10 + number % 10
-            print(number)
-
+            
 def main():
     card_number='4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '',' ': ''})
