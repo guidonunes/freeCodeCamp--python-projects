9da04ebff3c59b1422e71ac7fe0a1d2b1faf398f
guilherme2506 <guilherme.augd@gmail.com>
2024-01-23 21:30:45 -0300

    updated card number variable & removed print total call

--- 02_luhn_algorithm/main.py
+++ 02_luhn_algorithm/main.py
@@ -14,14 +14,13 @@ def verify_card_number(card_number):
         sum_of_even_digits += number
     total = sum_of_odd_digits + sum_of_even_digits
     return 0 == total % 10
-    print(total)
       
 def main():
-    card_number='4111-1111-4555-1141'
+    card_number='4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '',' ': ''})
     translated_card_number = card_number.translate(card_translation)
     if verify_card_number(translated_card_number):
         print("VALID!")
     else:
-        print("INVALID!")
+        print("INVALID")
 main()
