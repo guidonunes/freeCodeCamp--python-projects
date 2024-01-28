b89cc1f6d335a6491637d4c72f3859247ee56b39
guilherme2506 <guilherme.augd@gmail.com>
2024-01-28 20:02:03 -0300

    call the filter method on the lambda function within filter expenses by category function

--- 03_expense_tracker/main.py
+++ 03_expense_tracker/main.py
@@ -9,7 +9,7 @@ def total_expenses(expenses):
     return sum(map(lambda expense: expense['amount'], expenses))
 
 def filter_expenses_by_category(expenses, category):
-    lambda expense: expense['category'] == category
+    filter(lambda expense: expense['category'] == category, expenses)
 
 
 expenses = []
