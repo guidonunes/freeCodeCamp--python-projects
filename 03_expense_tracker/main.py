ce0c63d25ae8cde3b3dd8f66a8d6a434e8fbb0c1
guilherme2506 <guilherme.augd@gmail.com>
2024-01-28 19:30:53 -0300

    called map method on the lambda function

--- 03_expense_tracker/main.py
+++ 03_expense_tracker/main.py
@@ -6,7 +6,7 @@ def print_expenses(expenses):
         print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
 
 def total_expenses(expenses):
-    lambda expense: expense['amount']
+    map(lambda expense: expense['amount'])
 
 
 
