70e0fa9ba856a3a4032ccc93ad39517bc81f3e15
guilherme2506 <guilherme.augd@gmail.com>
2024-01-31 16:36:28 -0300

    added for statement after else clause

--- 04_case_converter/main.py
+++ 04_case_converter/main.py
@@ -12,6 +12,7 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     snake_cased_char_list = [
         '_' + char.lower() if char.isupper()
         else char
+        for char in pascal_or_camel_cased_string
     ]
 
 def main():
