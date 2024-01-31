48c2977eb3bdd78ce1ee510dcd5e16dae694a392
guilherme2506 <guilherme.augd@gmail.com>
2024-01-31 14:31:12 -0300

    added an else clause to the if statement

--- 04_case_converter/main.py
+++ 04_case_converter/main.py
@@ -4,3 +4,5 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
         if char.isupper():
             converted_character = '_' + char.lower()
             snake_cased_char_list.append(converted_character)
+        else:
+            snake_cased_char_list.append(char)
