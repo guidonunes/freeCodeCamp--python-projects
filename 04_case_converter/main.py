c13db1d883c7291469810c189c3bbf5d884267d7
guilherme2506 <guilherme.augd@gmail.com>
2024-01-31 14:49:42 -0300

    added main function

--- 04_case_converter/main.py
+++ 04_case_converter/main.py
@@ -9,3 +9,6 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     snake_cased_string = ''.join(snake_cased_char_list)
     clean_snake_cased_string = snake_cased_string.strip('_')
     return clean_snake_cased_string
+
+def main():
+    pass
