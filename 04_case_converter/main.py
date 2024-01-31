42a001303ae8b660a3c5c700ee53ca82410a3253
guilherme2506 <guilherme.augd@gmail.com>
2024-01-31 14:17:56 -0300

    created covert character variable within if statement block

--- 04_case_converter/main.py
+++ 04_case_converter/main.py
@@ -2,4 +2,4 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     snake_cased_char_list = []
     for char in pascal_or_camel_cased_string:
         if char.isupper():
-            pass
+            converted_character = '_' + char.lower()
