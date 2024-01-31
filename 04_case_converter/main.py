3947c4b499f59782228e356e337ff98a50479392
guilherme2506 <guilherme.augd@gmail.com>
2024-01-31 16:32:45 -0300

    added else clause within the list assigned to the snake cased char list variable

--- 04_case_converter/main.py
+++ 04_case_converter/main.py
@@ -11,6 +11,7 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     #return clean_snake_cased_string
     snake_cased_char_list = [
         '_' + char.lower() if char.isupper()
+        else char
     ]
 
 def main():
