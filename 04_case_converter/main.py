39e13a45f39a23df94b2f776c3b4ba9a21f214f9
guilherme2506 <guilherme.augd@gmail.com>
2024-02-01 09:47:03 -0300

    added a return statement to the convert snake case function

--- 04_case_converter/main.py
+++ 04_case_converter/main.py
@@ -14,6 +14,7 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
         else char
         for char in pascal_or_camel_cased_string
     ]
+    return ''.join(snake_cased_char_list).strip('_')
 
 def main():
     print(convert_to_snake_case('aLongAndComplexString'))
