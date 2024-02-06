156e3d1d95fd44ba8e43c9670e0232e1e48425f9
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-06 15:33:40 -0300

    add constraints variable

--- 05_password_generator/main.py
+++ 05_password_generator/main.py
@@ -16,6 +16,7 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         for _ in range(length):
             password += secrets.choice(all_characters)
             return password
+        constraints =[]
 
 #new_password = generate_password(8)
 #print(new_password)
