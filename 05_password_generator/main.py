c5f9f0f33aa7b27f5ec67642bac904e6d6856632
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-06 14:57:08 -0300

    declare generate password function & indented code within it

--- 05_password_generator/main.py
+++ 05_password_generator/main.py
@@ -1,11 +1,12 @@
 import secrets
 import string
 
+def generate_password():
 #Define the possible characters for the password
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
 
 #Combine all characters
-all_characters = letters + digits + symbols
+    all_characters = letters + digits + symbols
 
