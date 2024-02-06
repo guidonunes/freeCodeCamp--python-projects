f98833aea86ad7f0183e409152059514b6213bc6
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-06 16:22:52 -0300

    added caret to value assigned

--- 05_password_generator/main.py
+++ 05_password_generator/main.py
@@ -21,7 +21,7 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
 
 #new_password = generate_password(8)
 #print(new_password)
-pattern = '[a-z]t'
+pattern = '[^a-z]t'
 quote = "Not all those who wander are lost."
 print(re.findall(pattern, quote))
 
