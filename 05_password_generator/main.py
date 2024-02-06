52db84a4a46f9f0b8a1246af01e72862b59e0899
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-06 15:38:14 -0300

    add tuple to list assigned to constraints var

--- 05_password_generator/main.py
+++ 05_password_generator/main.py
@@ -16,7 +16,7 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         for _ in range(length):
             password += secrets.choice(all_characters)
             return password
-        constraints =[]
+        constraints =[(nums, '')]
 
 #new_password = generate_password(8)
 #print(new_password)
