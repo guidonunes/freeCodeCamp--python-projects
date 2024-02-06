a59126b05526002b005efb2df72f041b71308b21
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-06 15:08:40 -0300

    modified variable in the for loop

--- 05_password_generator/main.py
+++ 05_password_generator/main.py
@@ -11,6 +11,6 @@ def generate_password(length):
     all_characters = letters + digits + symbols
     password= ""
 #Generate password
-    for i in range(length):
+    for _ in range(length):
         password += secrets.choice(all_characters)
 
