fffe62bc76ffef1cb14f723aed73cbd4dcb7ee36
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-06 16:06:43 -0300

    modified pattern variable

--- 05_password_generator/main.py
+++ 05_password_generator/main.py
@@ -21,7 +21,7 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
 
 #new_password = generate_password(8)
 #print(new_password)
-pattern = re.compile('l+')
+pattern = 'l+'
 quote = "Not all those who wander are lost."
 print(pattern.search(quote))
 
