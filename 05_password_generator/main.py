3b47f0a47a727827eb8d04d6445efd19afa628d8
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-08 15:56:06 -0300

    compare contraint and the length of findall()

--- 05_password_generator/main.py
+++ 05_password_generator/main.py
@@ -2,31 +2,33 @@ import re
 import secrets
 import string
 
+
 def generate_password(length, nums, special_chars, uppercase, lowercase):
-#Define the possible characters for the password
+    # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
     symbols = string.punctuation
 
-#Combine all characters
+    # Combine all characters
     all_characters = letters + digits + symbols
-    
+
     while True:
-        password= ""
-#Generate password
+        password = ''
+        # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
-            return password
-        constraints =[
+       
+        constraints = [
             (nums, r'\d'),
             (lowercase, r'[a-z]'),
-            (uppercase, r'[A-Z]'),
-            (special_chars, fr'[{symbols}]') 
-            ]
+            (uppercase, r'[A-Z]'),            
+            (special_chars, fr'[{symbols}]')            
+        ]
         #Check constraints
         for constraint, pattern in constraints:
-            len(re.findall(pattern, password))
-                    
+            constraint <= len(re.findall(pattern, password))
+        
+        return password             
 #new_password = generate_password(8)
 #print(new_password)
 
