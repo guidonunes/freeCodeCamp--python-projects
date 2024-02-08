b8f2895664fcccfaadd7ab74b0fd2f13776a1c83
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-08 14:50:47 -0300

    add comment below constraints variable

--- 05_password_generator/main.py
+++ 05_password_generator/main.py
@@ -23,7 +23,8 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
             (uppercase, r'[A-Z]'),
             (special_chars, fr'[{symbols}]') 
             ]
-
+            #Check constraints
+            
 #new_password = generate_password(8)
 #print(new_password)
 
