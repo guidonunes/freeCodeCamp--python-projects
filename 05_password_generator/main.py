5a092cae308cae7594f3e73af6605d39bd8808e6
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-14 14:49:12 -0300

    update argument keywords

--- 05_password_generator/main.py
+++ 05_password_generator/main.py
@@ -32,7 +32,7 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         
     return password             
 
-new_password = generate_password(8,1,1,1,1)
+new_password = generate_password(length=8, nums=1, special_chars=1, uppercase=1, lowercase=1)
 print(new_password)
 
 
