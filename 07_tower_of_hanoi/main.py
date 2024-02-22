a57e3a87348980933c54520d5b21131588282e06
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-22 04:21:46 -0300

    add elif & else conditional to if statement

--- 07_tower_of_hanoi/main.py
+++ 07_tower_of_hanoi/main.py
@@ -13,7 +13,11 @@ def move(n, source, auxiliary, target):
     for i in range(number_of_moves):
        remainder = (i + 1) % 3
        if remainder == 1:
-           print(f'Move {i + 1} allowed between {source} and {target}')
+            print(f'Move {i + 1} allowed between {source} and {target}')
+       elif remainder == 2:
+            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+       else:
+            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
 
 #initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
