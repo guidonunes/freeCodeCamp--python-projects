30ecf4a6b90132dc93c18c3f4b1f79160da8f598
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-22 04:57:40 -0300

    implement append & pop method

--- 07_tower_of_hanoi/main.py
+++ 07_tower_of_hanoi/main.py
@@ -21,7 +21,7 @@ def move(n, source, auxiliary, target):
                 forward = True
             if forward:
                 print(f'Moving disk {rods[source][-1]} from {source} to {target}')
-                
+                rods[target].append(rods[source].pop())
        elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
        elif remainder == 0:
