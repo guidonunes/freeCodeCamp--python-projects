987c08d132e106b74fc9c2bde109c1f4c8e60146
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-22 04:44:13 -0300

    add if statement to check when forward is true

--- 07_tower_of_hanoi/main.py
+++ 07_tower_of_hanoi/main.py
@@ -19,6 +19,8 @@ def move(n, source, auxiliary, target):
                 forward = True
             elif rods[source] and rods[source][-1] < rods[target][-1]:
                 forward = True
+            if forward == True:
+                print(f'Moving disk {rods[source][-1]} from {source} to {target}')
        elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
        elif remainder == 0:
