506891d516002d47c2f22bef0f83169fadfd9713
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-22 07:13:49 -0300

    refactor code

--- 07_tower_of_hanoi/main.py
+++ 07_tower_of_hanoi/main.py
@@ -1,24 +1,21 @@
 NUMBER_OF_DISKS = 3
 
-
-rods = {
-    'A': list(range(NUMBER_OF_DISKS,0,-1)),
-    'B': [],
-    'C': []
-}
-
+A = list(range(NUMBER_OF_DISKS, 0, -1))
+B = []
+C = []
 
 def move(n, source, auxiliary, target):
     if n > 0:
-    #move n - 1 disks from source to auxiliary, so they are out of the way
+        # Move n - 1 disks from source to auxiliary, so they are out of the way
         move(n - 1, source, target, auxiliary)
-    #move the nth disk from source to target
-        rods[target].append(rods[source].pop())
-    #display our progress
-        print(rods,'\n')
-    #move the n - 1 disks that we left on auxiliary onto target
-        move(n - 1,  auxiliary, source, target)
-    
+        
+        # Move the nth disk from source to target
+        target.append(source.pop())
+        
+        # Display our progress
+        print( A, B, C, '\n')
+        
+        # Move the n - 1 disks that we left on auxiliary onto target
+        move(n - 1, auxiliary, source, target)
 
-#initiate call from source A to target C with auxiliary B
-move(NUMBER_OF_DISKS, 'A', 'B', 'C')
+move(NUMBER_OF_DISKS, A, B, C)
