ddea58c3bab63c0e49a87223fbaaea1393800fd7
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-22 05:41:22 -0300

    call make allowed move function

--- 07_tower_of_hanoi/main.py
+++ 07_tower_of_hanoi/main.py
@@ -29,6 +29,7 @@ def move(n, source, auxiliary, target):
        remainder = (i + 1) % 3
        if remainder == 1:
             print(f'Move {i + 1} allowed between {source} and {target}')
+            make_allowed_move(source, target)   
        elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
        elif remainder == 0:
