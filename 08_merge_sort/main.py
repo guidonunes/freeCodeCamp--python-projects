535865c8c4ac084a3ce7cc0ecd5f1acd32c70330
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-24 17:38:16 -0300

    recursively called merge sort function

--- 08_merge_sort/main.py
+++ 08_merge_sort/main.py
@@ -1,4 +1,5 @@
 def merge_sort(array):
     middle_point = array // 2
     left_part = array[:middle_point]
-    right_part = array[middle_point]
+    right_part = array[middle_point:]
+    merge_sort()
