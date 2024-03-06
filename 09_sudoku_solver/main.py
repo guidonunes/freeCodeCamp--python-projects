2cfdff82bd8f7a2d60a96c17ecf4f584b5a05f00
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-05 22:46:24 -0300

    add return statement

--- 09_sudoku_solver/main.py
+++ 09_sudoku_solver/main.py
@@ -30,3 +30,5 @@ class Board:
                     board_string += middle_lines
             else:
                 board_string += lower_lines
+            #return string containing the complete visual representation of the sudoku board
+            return board_string
