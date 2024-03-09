a6284997d7f0a05075f94ca4d6df6857b03836f4
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-09 12:06:11 -0300

    nested for loop to iterate over columns

--- 09_sudoku_solver/main.py
+++ 09_sudoku_solver/main.py
@@ -58,4 +58,5 @@ class Board:
         row_start = (row//3) * 3
         col_start = (col//3) * 3
         for row_no in range(row_start, row_start + 3):
-            pass
+            for col_no in range(col_start, col_start + 3):
+                pass
