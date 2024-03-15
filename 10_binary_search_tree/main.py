ddd702a8339e84f8e2d691802fa1831168222b59
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-15 17:51:35 -0300

    add return statement

--- 10_binary_search_tree/main.py
+++ 10_binary_search_tree/main.py
@@ -20,3 +20,4 @@ class BinarySearchTree:
             node.left = self._insert(node.left, key)
         elif key > node.key:
             node.right = self._insert(node.right, key)
+        return node
