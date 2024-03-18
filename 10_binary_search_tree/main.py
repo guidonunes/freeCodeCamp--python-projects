6e7070ba5b3098151481b5d359344be1d3e30fd3
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-18 16:36:35 -0300

    add return statement calling search method

--- 10_binary_search_tree/main.py
+++ 10_binary_search_tree/main.py
@@ -30,4 +30,5 @@ class BinarySearchTree:
             return node
         if key < node.key:
             return self._search(node.left, key)
+        return self._search(node.right, key)
             
