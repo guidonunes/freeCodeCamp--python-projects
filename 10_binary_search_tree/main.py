02ba3cdc43e411883f7933e36582aeba17aa5e1c
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-20 16:57:04 -0300

    set variable to recursively delete the node from the right subtree

--- 10_binary_search_tree/main.py
+++ 10_binary_search_tree/main.py
@@ -48,4 +48,5 @@ class BinarySearchTree:
                 return node.right
             elif node.right is None:
                 return node.left
-            node.key = self._min_value(node.right)          
+            node.key = self._min_value(node.right)
+            node.right = self._delete(node.right, key)
