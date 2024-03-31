a4e0eb39c0c257da7be5c801698c61dc6918181c
Guilherme Nunes <guilherme.augd@gmail.com>
2024-03-31 11:38:40 -0300

    add class

--- 11_arithmetic_formatter/main.py
+++ 11_arithmetic_formatter/main.py
@@ -1,5 +1,7 @@
-def arithmetic_arranger(problems, show_answers=False):
+class ArithmeticFormatter:
+    
+    def arithmetic_arranger(problems, show_answers=False):
 
-    return problems
+        return problems
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+    print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
