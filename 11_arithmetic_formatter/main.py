c52140fb07ed62243f902a98ca62748a3a7f3fdd
Guilherme Nunes <guilherme.augd@gmail.com>
2024-04-02 15:21:16 -0300

    add method to store variables

--- 11_arithmetic_formatter/main.py
+++ 11_arithmetic_formatter/main.py
@@ -2,14 +2,18 @@ class ArithmeticFormatter:
     def __init__(self):
         self.reset()
     
+    def reset(self):
+        self.top_int = ""
+        self.bottom_int = ""
+        self.line_format = ""
+        self.result= ""
+
     def arithmetic_arranger(problems, show_answers=False):
+
     #max number of problems
         if len(problems) > 5: 
             return "Error: Too many problems."
-        top_int = ""
-        bottom_int = ""
-        line_format = ""
-        result = ""
+
 
 
         return problems
