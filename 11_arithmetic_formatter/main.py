e25d74fbb369a4b8f55ea81f7f0dab90d9e914a3
Guilherme Nunes <guilherme.augd@gmail.com>
2024-04-02 15:09:51 -0300

    add init method

--- 11_arithmetic_formatter/main.py
+++ 11_arithmetic_formatter/main.py
@@ -1,9 +1,16 @@
 class ArithmeticFormatter:
+    def __init__(self):
+        self.reset()
     
     def arithmetic_arranger(problems, show_answers=False):
     #max number of problems
         if len(problems) > 5: 
             return "Error: Too many problems."
+        top_int = ""
+        bottom_int = ""
+        line_format = ""
+        result = ""
+
 
         return problems
 
